297c349be56b112002b14fe5b84ce3d331cd1960
Mayankmishra110 <kmmay15@gmail.com>
2024-02-08 23:13:16 +0530

    practice

--- main.js
+++ main.js
@@ -221,3 +221,40 @@ function isAnagram(str1, str2) {
 
   return sortedStr(str1) === sortedStr(str2);
 }
+class Animal {
+  constructor(name, legCount) {
+    this.name = name;
+    this.legCount = legCount;
+  }
+  describe() {
+    return `${this.name} has ${this.legCount} legs`;
+  }
+}
+
+function mathMethods(value) {
+  console.log("Original Value:", value);
+
+  let rounded = Math.round(value);
+  console.log("After round():", rounded);
+
+  let ceiling = Math.ceil(value);
+  console.log("After ceil():", ceiling);
+
+  let flooring = Math.floor(value);
+  console.log("After floor():", flooring);
+
+  let randomValue = Math.random();
+  console.log("After random():", randomValue);
+
+  let maxValue = Math.max(5, 10, 15);
+  console.log("After max():", maxValue);
+
+  let minValue = Math.min(5, 10, 15);
+  console.log("After min():", minValue);
+
+  let powerOfTwo = Math.pow(value, 2);
+  console.log("After pow():", powerOfTwo);
+
+  let squareRoot = Math.sqrt(value);
+  console.log("After sqrt():", squareRoot);
+}
